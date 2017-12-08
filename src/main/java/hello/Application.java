bf1bd56d8b65af7b22bae6bf04565794dbb06eb2
victoriawood <wood.victoriaf@gmail.com>
2017-12-07 22:17:05 -0500

    cmt

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,14 +10,6 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-      .addResourceHandler("/resources/**")
-      .addResourceLocations("/resources/")
-      .setCachePeriod(3600)
-      .resourceChain(true)
-      .addResolver(new PathResourceResolver());
-}
+
 
 }
